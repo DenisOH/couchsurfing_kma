d7795d8d2d1a7ae0fd0d1ec852c01803b2361517
Denys Halenok <denys.halenok@gmail.com>
2020-02-26 18:18:27 +0200

    Updates

---
+++ precache-manifest.083fb79c5a66e7c8539c3dbf1879906b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e6868d083dbfae1cae8e11ea098d6e",
+    "url": "/couchsurfing_kma/index.html"
+  },
+  {
+    "revision": "fd73121ac9916d4de806",
+    "url": "/couchsurfing_kma/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97e86aad5904335d3eb4",
+    "url": "/couchsurfing_kma/static/js/2.f4b01e20.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/couchsurfing_kma/static/js/2.f4b01e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd73121ac9916d4de806",
+    "url": "/couchsurfing_kma/static/js/main.884cd5d1.chunk.js"
+  },
+  {
+    "revision": "abec9312d606076ca587",
+    "url": "/couchsurfing_kma/static/js/runtime-main.3859c8bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/couchsurfing_kma/static/media/logo.5d5d9eef.svg"
+  }
+]);
