9ff697b8fe8fd11755e54ef0e859dd6d3439ec63
Denys Halenok <denys.halenok@gmail.com>
2020-02-27 00:36:39 +0200

    Hotfix react-router basename; style fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import App from './App';
 import CssBaseline from "@material-ui/core/CssBaseline";
 
